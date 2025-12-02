0b38dc255c051aa6023904ca51a3de02bcea05d5
Shailesh Pandavadra <pandavadra.shailesh@outlook.com>
2025-12-02 15:28:59 +0530

    Section 3 - Category Edit Get Action

--- Rocky-DOTNET_10/Controllers/CategoryController.cs
+++ Rocky-DOTNET_10/Controllers/CategoryController.cs
@@ -37,5 +37,21 @@ namespace Rocky_DOTNET_10.Controllers
             }
             return View(obj);
         }
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
