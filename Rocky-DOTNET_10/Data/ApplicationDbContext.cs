8a37702c64c7fd6b70d00302bdbd6996fd50240b
Shailesh Pandavadra <pandavadra.shailesh@outlook.com>
2025-12-02 12:44:10 +0530

    Section 2 - Setup DbContext

---
+++ Rocky-DOTNET_10/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Rocky_DOTNET_10.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
