6f7658ed35807567a6bb014dc38482cf0fc16694
Shailesh Pandavadra <pandavadra.shailesh@outlook.com>
2025-12-02 13:28:09 +0530

    Section 2 - Get all Category

--- Rocky-DOTNET_10/Controllers/CategoryController.cs
+++ Rocky-DOTNET_10/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rocky_DOTNET_10.Data;
+using Rocky_DOTNET_10.Models;
 
 namespace Rocky_DOTNET_10.Controllers
 {
     public class CategoryController : Controller
     {
+
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
         }
     }
 }
