582ad36dfd88dd2014c929bac3c0d8e3a67fe24a
Shailesh Pandavadra <pandavadra.shailesh@outlook.com>
2025-12-02 13:54:05 +0530

    Section 2 - Add Category Part 2

--- Rocky-DOTNET_10/Models/Category.cs
+++ Rocky-DOTNET_10/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Rocky_DOTNET_10.Models
 {
@@ -7,6 +8,7 @@ namespace Rocky_DOTNET_10.Models
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
